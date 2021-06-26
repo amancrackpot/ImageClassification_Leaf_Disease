62917d1bdbfaaeb9f4f24c5a0ab71d3fa59d0068
amancrackpot <59706418+amancrackpot@users.noreply.github.com>
2021-06-26 18:16:18 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -13,6 +13,7 @@ import pdb
 from pathlib import Path
 import numpy as np
 
+
 export_file_name = 'final_model.h5'
 classes = ['Cassava Bacterial Blight (CBB)', 'Cassava Mosaic Disease (CMD)', 'Cassava Brown Streak Disease (CBSD)', 'Cassava Green Mottle (CGM)', 'Healthy']
 path = Path(__file__).parent
@@ -28,17 +29,10 @@ app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Reques
 app.mount('/static', StaticFiles(directory='src/static'))
 
 
-async def setup_learner():
-    fin_model = tf.keras.models.load_model(path/'saved'/export_file_name)
-    return fin_model
+fin_model = tf.keras.models.load_model(path/'saved'/export_file_name)
+graph = tf.get_default_graph()
 
-loop = asyncio.get_event_loop()
-tasks = [asyncio.ensure_future(setup_learner())]
-fin_model = loop.run_until_complete(asyncio.gather(*tasks))[0]
-loop.close()
-
-	
-async def model_predict(img_b):
+def model_predict(img_b):  
     image = PIL.Image.open(BytesIO(img_b))
     image_numpy = np.expand_dims(np.array(image), 0) #add batch dim
     # these 2 will be the only preprocessing steps required
